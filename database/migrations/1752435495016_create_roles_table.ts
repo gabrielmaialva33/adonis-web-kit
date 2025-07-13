099cc1348b866e92d9c7eaabae1a5099d320712a
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-07-13 17:04:42 -0300

    feat(database): add name and slug columns to roles table

--- database/migrations/1752435495016_create_roles_table.ts
+++ database/migrations/1752435495016_create_roles_table.ts
@@ -6,6 +6,8 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.string('name').notNullable()
+      table.string('slug').notNullable().unique()
 
       table.timestamp('created_at')
       table.timestamp('updated_at')
