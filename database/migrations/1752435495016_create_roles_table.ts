c208708f4b434be90018a320bb61fa9b27736a61
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-07-13 17:06:29 -0300

    feat(database): add description column to roles table and update timestamp defaults

--- database/migrations/1752435495016_create_roles_table.ts
+++ database/migrations/1752435495016_create_roles_table.ts
@@ -7,10 +7,11 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.string('name').notNullable()
+      table.text('description').nullable()
       table.string('slug').notNullable().unique()
 
-      table.timestamp('created_at')
-      table.timestamp('updated_at')
+      table.timestamp('created_at', { useTz: true }).notNullable().defaultTo(this.now())
+      table.timestamp('updated_at', { useTz: true }).notNullable().defaultTo(this.now())
     })
   }
 
