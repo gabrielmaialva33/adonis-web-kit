faa079b9ae32015c76c3ba2e63bd8be49c8ca023
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-07-13 14:22:53 -0300

    feat(rcfile): add limiter provider to adonisrc
    Included `@adonisjs/limiter/limiter_provider` in the `adonisrc.ts` file to enable the rate-limiting functionality. Aligns with the newly introduced limiter configurations and enhancements.

--- adonisrc.ts
+++ adonisrc.ts
@@ -54,6 +54,7 @@ export default defineConfig({
     () => import('@adonisjs/auth/auth_provider'),
     () => import('@adonisjs/inertia/inertia_provider'),
     () => import('@adonisjs/i18n/i18n_provider'),
+    () => import('@adonisjs/limiter/limiter_provider'),
   ],
 
   /*
