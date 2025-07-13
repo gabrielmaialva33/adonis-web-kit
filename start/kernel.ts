35b5930c4f3fa7ace17dc1289bf31ebc8ac88ddc
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-07-13 17:41:36 -0300

    refactor(kernel): remove duplicate middleware imports

--- start/kernel.ts
+++ start/kernel.ts
@@ -39,9 +39,6 @@ router.use([
   () => import('@adonisjs/session/session_middleware'),
   () => import('@adonisjs/shield/shield_middleware'),
   () => import('@adonisjs/auth/initialize_auth_middleware'),
-  () => import('@adonisjs/session/session_middleware'),
-  () => import('@adonisjs/shield/shield_middleware'),
-
   () => import('#middleware/detect_user_locale_middleware'),
 ])
 
