ca75b4721d41d61b7c07065d10eb66e0b4503168
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-07-13 14:20:21 -0300

    feat(interfaces): add PermissionInterface for resource actions
    Introduced `PermissionInterface` to define available resources and actions for permission management. This standardizes access control definitions across the application.

---
+++ app/interfaces/permission_interface.ts
@@ -0,0 +1,15 @@
+namespace IPermission {
+  export enum Resources {
+    USERS = 'users',
+  }
+
+  export enum Actions {
+    LIST = 'list',
+    READ = 'read',
+    CREATE = 'create',
+    UPDATE = 'update',
+    DELETE = 'delete',
+  }
+}
+
+export default IPermission
